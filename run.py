482f2ad8c4d053a39e62abcbbe0224313a76bd27
BrrunaRibeiro <brrunarib@gmail.com>
2023-10-05 13:19:04 +0200

    Add calculated surplus data to the surplus sheet

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
 from pprint import pprint
+
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -55,6 +56,16 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated sucessfully.\n")
 
+
+def update_surplus_worksheet(data):
+    """
+    Update surplus worksheet, add new row with the list data rovided.
+    """
+    print("Updating surplus data..\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(data)
+    print("Surplus worksheet updated sucessfully.\n")  
+
 def calculate_surplus_data(sales_row):
     """
     Compare the sales with stock and calculate the surplus for each item type.
@@ -66,7 +77,14 @@ def calculate_surplus_data(sales_row):
     print("calculating surplus data..\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
+    
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+    
+    return surplus_data
+
 
 def main():
     """
@@ -75,7 +93,7 @@ def main():
 data = get_sales_data()
 sales_data = [int(num) for num in data]
 update_sales_worksheet(sales_data)
-calculate_surplus_data(sales_data)
-
+new_surplus_data = calculate_surplus_data(sales_data)
+update_surplus_worksheet(new_surplus_data)
 
 main()
